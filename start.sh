4aef79d9f25f1e58a8c3be8ce8b52d291a4faf1e
Alexandre Bulté <alexandre@bulte.net>
2020-10-28 18:00:03 +0100

    Install udata-tabular-preview

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 pip install -r /srv/udata/requirements/develop.pip
 pip install -e /srv/udata/
 pip install -e /srv/udata-gouvfr
+pip install udata-tabular-preview
 udata db migrate
 udata search index
 udata generate-fixtures
