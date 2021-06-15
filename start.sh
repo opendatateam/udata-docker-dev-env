513a5d3e816933c5feae603045177b8d3676ccb0
quaxsze <service@plum.eu.com>
2021-06-15 12:25:15 +0200

    removed tabular

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 pip install -r /srv/udata/requirements/develop.pip
 pip install -e /srv/udata/
 pip install -e /srv/udata-gouvfr
-pip install udata-tabular-preview
 udata db migrate
 udata search index
 udata generate-fixtures
